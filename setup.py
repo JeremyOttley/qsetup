41a2d0e0db7148d57e62eb5d08a39fbaa304705b
Jeremy Ottley <Gazaunga@gmail.com>
2019-04-19 09:00:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ HOME = os.getenv('HOME') + "/"
 SCRIPT_DIR = os.path.realpath(__file__)
 CONFIG_DIR = os.getenv('XDG_CONFIG_HOME') + "/"
 
+# Create directory if it does not exist already
+if not os.path.exists(CONFIG_DIR + "qtile/"):
+  os.makedirs(CONFIG_DIR + "qtile/")
+
 ##: Step 1: ensure that python3 is installed
 def check():
 
