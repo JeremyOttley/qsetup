4dc0464894ad49328a7b8acd81bb250e8c7314ef
Jeremy Ottley <Gazaunga@gmail.com>
2019-04-19 08:58:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ import sys
 import shutil
 import subprocess
 
+HOME = os.getenv('HOME') + "/"
+SCRIPT_DIR = os.path.realpath(__file__)
+CONFIG_DIR = os.getenv('XDG_CONFIG_HOME') + "/"
+
 ##: Step 1: ensure that python3 is installed
 def check():
 
